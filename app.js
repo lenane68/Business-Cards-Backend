bbf32e0e47b679a0756be02206b18291e989297a
lena <lena.mhamed@hotmail.com>
2025-06-12 10:48:24 +0300

    app correction

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 import express from "express";
-import mongoose from "mongoose";
 import cors from "cors";
+import mongoose from "mongoose";
 import usersRouter from "./routes/users.js";
 import authRouter from "./routes/auth.js";
 import cardsRouter from "./routes/cards.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
@@ -16,9 +16,12 @@ app.use("/api/users", usersRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/cards", cardsRouter);
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ Connected to MongoDB"))
-  .catch(err => console.error("❌ MongoDB error:", err));
+app.use((req, res) => res.status(404).send("Endpoint not found"));
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ Connected to MongoDB…"))
+  .catch(console.error);
 
 const port = process.env.PORT || 8000;
-app.listen(port, () => console.log(`🚀 Backend running on port ${port}`));
+app.listen(port, () => console.log(`🚀 Backend running on port ${port}…`));
