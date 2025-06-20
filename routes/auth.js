c42bee5ebff6c0f6ee0601b4d5a5887c89d5cc18
lena <lena.mhamed@hotmail.com>
2025-06-20 18:45:12 +0300

    new auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,21 @@
 import express from "express";
-import User from "../models/User.js";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import User from "../models/User.js";
 
 const router = express.Router();
 
 router.post("/", async (req, res) => {
   const { email, password } = req.body;
-  const user = await User.findOne({ email, password });
+
+  const user = await User.findOne({ email });
   if (!user) return res.status(400).send("Invalid credentials");
+
+  const valid = await bcrypt.compare(password, user.password);
+  if (!valid) return res.status(400).send("Invalid credentials");
+
   const token = jwt.sign({ _id: user._id, role: user.role }, process.env.JWT_KEY);
-  res.send(token);
+  res.send({ token });
 });
 
 export default router;
