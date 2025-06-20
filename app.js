3e65a5bbcf09841ff716b52f11760a60bede6cd6
lena <lena.mhamed@hotmail.com>
2025-06-20 19:26:35 +0300

    update app

--- app.js
+++ app.js
@@ -15,9 +15,10 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("✅ Connected to MongoDB..."))
-  .catch((err) => console.error("❌ Could not connect to MongoDB...", err));
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ Connected to MongoDB…"))
+  .catch(err => console.error("❌ Could not connect to MongoDB…", err));
+
 
 app.use("/api/users", usersRouter);
 app.use("/api/auth", authRouter);
